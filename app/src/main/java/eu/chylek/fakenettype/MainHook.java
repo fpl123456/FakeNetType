a18ae7241068fc742b414720df14d9ffe1c90110
fpl123456 <fpl123456@users.noreply.github.com>
2026-05-13 19:51:34 +0800

    fix: XC_LoadPackage import path - use de.robv.android.xposed not callbacks subpackage

--- app/src/main/java/eu/chylek/fakenettype/MainHook.java
+++ app/src/main/java/eu/chylek/fakenettype/MainHook.java
@@ -15,7 +15,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XC_LoadPackage;
 
 /**
  * FakeNetType - Xposed/LSPosed 模块
